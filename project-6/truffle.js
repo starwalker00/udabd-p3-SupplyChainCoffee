0889f355f17d8011e9ed353d93cdc7a9d40af21a
starwalker00 <aaronwalkerup@gmail.com>
2021-08-30 21:01:36 +0200

    truffle rinkeby config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const fs = require('fs');
+const privatekey = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(privatekey, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
     }
   }
 };
